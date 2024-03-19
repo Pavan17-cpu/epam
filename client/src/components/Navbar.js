76f192f91532ba383dc4ccc103e6b4b516fe81c8
Pavan17-cpu <110300098+Pavan17-cpu@users.noreply.github.com>
2024-03-19 19:17:01 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../actions/userActions";
-export default function Navbar() {
-  const cartreducer = useSelector((state) => state.cartReducer);
 
-  const { cartItems } = cartreducer;
+export default function Navbar() {
+  const cartReducer = useSelector((state) => state.cartReducer);
+  const { cartItems } = cartReducer;
 
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -16,7 +16,7 @@ export default function Navbar() {
         <a className="navbar-brand" href="/">
           e-comm
         </a>
-        
+
         <button
           className="navbar-toggler"
           type="button"
@@ -26,11 +26,10 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          
-          <span className="navbar-toggler-icon"><i class="fas fa-bars" style={{color:'white'}}></i></span>
+          <span className="navbar-toggler-icon">
+            <i class="fas fa-bars" style={{ color: "white" }}></i>
+          </span>
         </button>
-
-
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="navbar-nav ml-auto">
@@ -39,25 +38,27 @@ export default function Navbar() {
                 <button
                   className="btn btn-secondary dropdown-toggle"
                   type="button"
-
                   id="dropdownMenuButton"
                   data-toggle="dropdown"
                   aria-haspopup="true"
                   aria-expanded="false"
                 >
-                  <i style={{color:'white'}} className="fa fa-user" aria-hidden="true"></i> {currentUser.name}
+                  <i style={{ color: "white" }} className="fa fa-user" aria-hidden="true"></i>{" "}
+                  {currentUser.name}
                 </button>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="dropdownMenuButton"
-                >
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                   <a className="dropdown-item" href="/profile">
                     Profile
                   </a>
                   <a className="dropdown-item" href="/orders">
                     Orders
                   </a>
-                  <li className="dropdown-item" onClick={()=>{dispatch(logoutUser())}}>
+                  {currentUser.isAdmin && (
+                    <a className="dropdown-item" href="/admin">
+                      Admin
+                    </a>
+                  )}
+                  <li className="dropdown-item" onClick={() => dispatch(logoutUser())}>
                     Logout <i class="fas fa-sign-out-alt"></i>
                   </li>
                 </div>
@@ -73,14 +74,13 @@ export default function Navbar() {
               <a className="nav-link" href="/contact">
                 Contact Us
               </a>
-              </li>
+            </li>
 
             <li className="nav-item">
               <a className="nav-link" href="/cart">
                 <i class="fas fa-shopping-cart"></i> {cartItems.length}
               </a>
             </li>
-            
           </div>
         </div>
       </nav>
